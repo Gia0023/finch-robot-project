be73e46ab85b0956b1f6686c2e20fa344710d1e9
Gia0023 <ggalvan0023@mymail.lausd.net>
2026-05-29 11:44:12 -0700

    added colors and movement

--- src/main/java/com/zippyzap/FinalFinch.java
+++ src/main/java/com/zippyzap/FinalFinch.java
@@ -6,4 +6,22 @@ public class FinalFinch extends Finch {
     public FinalFinch(String device) {
         super(device);
     }
-}
+
+    public void squareZippy(){
+        int count = 0;
+        while(count<4){
+            setMove("F",5,20);
+            setTurn("R",90,50);
+             count++;    
+        }
+    }
+
+    public void colorOn(){
+        setTail(1, 40, 20, 60);
+        setTail(2, 3, 73, 25);
+        setTail(3, 40, 20, 60);
+        setTail(4, 3, 73, 25);  
+        setBeak(255,255,0);  
+        }
+
+    }
--- src/main/java/com/zippyzap/TesterFinch.java
+++ src/main/java/com/zippyzap/TesterFinch.java
@@ -4,11 +4,12 @@ public class TesterFinch{
     public static void main(String[] args){
         FinalFinch zippy = new FinalFinch("A");
 
-         zippy.setMove("F", 20, 50);
-         zippy.setTail(1, 40, 20, 60);
-         zippy.setTail(2, 3, 73, 25);
-         zippy.setTail(3, 40, 20, 60);
-         zippy.setTail(4, 3, 73, 25);
-         zippy.setBeak(255,255,0);
+
+        zippy.squareZippy();
+         zippy.colorOn();
+         zippy.print("Hi");
+         zippy.playNote(80,0.5);
+         
+         zippy.stopAll();
     }
 }
